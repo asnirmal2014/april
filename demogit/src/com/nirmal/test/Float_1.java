5b11e6e1120d85e6dce5ecdeaf2091338ab4dac5
QCT <snkta2018@gmail.com>
2025-05-06 10:29:13 +0530

    New Commit

--- demogit/src/com/nirmal/test/Float_1.java
+++ demogit/src/com/nirmal/test/Float_1.java
@@ -4,9 +4,5 @@ public class Float_1 {
 	public static void main(String[] args) {
 		float f = 3.78541f;
 		System.out.println(f);
-		System.out.println(f);
-		System.out.println(f);
-		System.out.println(f);
-		System.out.println(f);
 	}
 }
